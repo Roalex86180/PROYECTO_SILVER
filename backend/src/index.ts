b2ab92c0a502ccb8ddfd1d6e467083a08fc3b2ef
Roger Alexander Reimi Vega <alexreimi@gamil.com>
2026-03-12 16:12:37 -0300

    fix base de datos1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,9 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import path from 'path'
 
-dotenv.config()
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
 
 import workersRouter   from './routes/workers'
 import companiesRouter from './routes/companies'
